8a1a8d9db050c5b8253837d74a8b50d7dd6c655f
GroupieSteven <StevenWANG0805@outlook.com>
2026-07-05 13:27:48 +0800

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return 0
+
+            left_height = dfs(node.left)
+            right_height = dfs(node.right)
+
+            if abs(left_height - right_height) > 1:
+                return -1
+
+            if left_height == -1 or right_height == -1:
+                return -1
+
+            return 1 + max(left_height, right_height)
+
+        return dfs(root) != -1
